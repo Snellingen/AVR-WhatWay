dd87b55d32c542407b4720218d0ceefd677f4b1a
Snellingen <matias@snellingen.com>
2014-11-21 17:12:19 +0100

    added text to game gui

--- GUIHandler.cpp
+++ GUIHandler.cpp
@@ -23,6 +23,11 @@ void GUIHandler::Update()
 
 void GUIHandler::DrawInGameGUI()
 {
-	tft->fillRoundRect(0, -5, DISPLAY_WIDTH, 20, 7, WW_COLOR_WHITE);
+	tft->fillRoundRect(0, -3, DISPLAY_WIDTH, 20, 7, WW_COLOR_WHITE);
 	tft->fillRoundRect(0, DISPLAY_HEIGHT - 15, DISPLAY_WIDTH, 20, 7, WW_COLOR_WHITE);
+	tft->setCursor(10, 5);
+	tft->setTextColor(WW_COLOR_DARKBLUE); 
+	tft->print("Score:");
+	tft->setCursor(10, DISPLAY_HEIGHT - 11);
+	tft->print("High Score:");
 }
