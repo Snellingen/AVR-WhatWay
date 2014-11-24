6c4d2abdf8be14618c548b6f6f57a86768424a9c
Snellingen <matias@snellingen.com>
2014-11-24 10:36:58 +0100

    added more global values so that the program uses less memory

--- GlobalValues.h
+++ GlobalValues.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <Arduino.h>
+
 #define WW_COLOR_WHITE 61342
 #define WW_COLOR_BLUE 13531
 #define WW_COLOR_DARKBLUE 10730
@@ -6,6 +8,11 @@
 #define WW_COLOR_GREEN 30000
 #define DISPLAY_WIDTH 128
 #define DISPLAY_HEIGHT 160
+
+// THIS IS TO SAVE MEMORY SPACE
+static uint16_t GLB_CurrentColor = WW_COLOR_WHITE; 
+static uint16_t GLB_CurrentClearColor = WW_COLOR_DARKBLUE;
+static uint8_t GLB_CurrentArrowWidth = 10; 
 
 enum GlobalDirection
 {
